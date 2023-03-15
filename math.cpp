d77a0a318737c79851537bc254ecb5520894e3f9
qosmio <79136983+qosmioo@users.noreply.github.com>
2023-03-15 23:01:09 +0300

    Update math.cpp
    removed operation index, added error flag

--- math.cpp
+++ math.cpp
@@ -1,18 +1,23 @@
 #include "math.h"
 
-double label_result (struct lbl_res calc, int &operations_index) {
-    double label;
+double label_result (struct lbl_res &calc) {
+    double label = 0;
     if (calc.operations_index == PLUS_IND) {
         label = (calc.first_num + calc.second_num);
     } else if (calc.operations_index == MINUS_IND) {
         label = (calc.first_num - calc.second_num);
     } else if (calc.operations_index == DIV_IND) {
-        label = (calc.first_num / calc.second_num);
+        if (calc.second_num != 0) {
+            label = (calc.first_num / calc.second_num);
+        } else {
+            calc.message = "error";
+        }
+        
    } else if (calc.operations_index == MULT_IND) {
         label = (calc.first_num * calc.second_num);
     }
 
-    operations_index = 0;
+    calc.operations_index = 0;
 
     return label;
 }
