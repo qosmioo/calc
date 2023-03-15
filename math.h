5a327152fd556cb44820ec01beefcbe9f8821f40
qosmio <79136983+qosmioo@users.noreply.github.com>
2023-03-15 23:01:49 +0300

    Update math.h

--- math.h
+++ math.h
@@ -4,13 +4,15 @@
 #define MINUS_IND 2
 #define DIV_IND 4
 #define MULT_IND 3
+#include "iostream"
 
 struct lbl_res{
     int operations_index;
     double first_num;
     double second_num;
+    std::string message; 
 };
 
-double label_result (struct lbl_res calc, int &operations_index);
+double label_result (struct lbl_res &calc);
 
 #endif // MATH_H
